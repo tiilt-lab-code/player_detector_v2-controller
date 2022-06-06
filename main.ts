1755a661309473eb6a1c37a01da6957e521e33b5
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2022-06-06 10:47:58 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,14 @@ enum RadioMessage {
     increase_db = 24409,
     decrease_db = 42436,
     kill_sound = 3114,
-    play_sound = 6322
+    play_sound = 6322,
+    toggle_pause = 52104
 }
-radio.onReceivedNumber(function (receivedNumber) {
-	
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    for (let index = 0; index <= 3; index++) {
+        radio.setGroup(index)
+        radio.sendMessage(RadioMessage.toggle_pause)
+    }
 })
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index <= 3; index++) {
@@ -14,37 +18,18 @@ input.onButtonPressed(Button.A, function () {
         radio.sendMessage(RadioMessage.play_sound)
     }
 })
-radio.onReceivedMessage(RadioMessage.kill_sound, function () {
-    music.setBuiltInSpeakerEnabled(false)
-    running = false
-    basic.showString("Stop")
-})
-radio.onReceivedMessage(RadioMessage.increase_db, function () {
-    dB_threshold += 5
-})
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index <= 3; index++) {
         radio.setGroup(index)
         radio.sendMessage(RadioMessage.kill_sound)
     }
 })
-radio.onReceivedMessage(RadioMessage.play_sound, function () {
-    music.setBuiltInSpeakerEnabled(true)
-    running = true
-    basic.showString("Go")
-})
-radio.onReceivedMessage(RadioMessage.decrease_db, function () {
-    dB_threshold += -5
-})
-let running = false
-basic.showString("Start-Stop")
+basic.showString("coach")
 basic.clearScreen()
+basic.showString("P")
 radio.setTransmitSerialNumber(true)
 radio.setTransmitPower(7)
 let serial_numbers: number[] = []
 let recent_time: number[] = []
 let dB_threshold = -65
-running = false
-basic.forever(function () {
-	
-})
+let running = false
