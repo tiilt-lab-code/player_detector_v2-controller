e2e4197d323cb07d7c640532c65837c31de86bb8
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2022-06-06 06:04:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ radio.onReceivedNumber(function (receivedNumber) {
 	
 })
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index <= 2; index++) {
+    for (let index = 0; index <= 3; index++) {
         radio.setGroup(index)
         radio.sendMessage(RadioMessage.play_sound)
     }
@@ -23,7 +23,7 @@ radio.onReceivedMessage(RadioMessage.increase_db, function () {
     dB_threshold += 5
 })
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index <= 2; index++) {
+    for (let index = 0; index <= 3; index++) {
         radio.setGroup(index)
         radio.sendMessage(RadioMessage.kill_sound)
     }
